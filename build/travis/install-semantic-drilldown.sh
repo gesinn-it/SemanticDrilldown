eaac309d222807e51c44df342005d56598a78033
Alexander Gesinn <alexander.gesinn@gesinn.it>
2021-02-10 12:19:42 +0100

    install phpunit in MW install path

--- build/travis/install-semantic-drilldown.sh
+++ build/travis/install-semantic-drilldown.sh
@@ -7,11 +7,11 @@ MW_INSTALL_PATH=$BASE_PATH/../mw
 cd $MW_INSTALL_PATH/extensions
 git clone https://gerrit.wikimedia.org/r/mediawiki/extensions/SemanticDrilldown.git
 
+cd ..
+echo 'require_once "$IP/extensions/SemanticDrilldown/SemanticDrilldown.php";' >> LocalSettings.php
+php maintenance/update.php --skip-external-dependencies --quick
+
 if [ "$PHPUNIT" != "" ]
 then
 	composer require 'phpunit/phpunit='$PHPUNIT --update-with-dependencies
 fi
-
-cd ..
-echo 'require_once "$IP/extensions/SemanticDrilldown/SemanticDrilldown.php";' >> LocalSettings.php
-php maintenance/update.php --skip-external-dependencies --quick
