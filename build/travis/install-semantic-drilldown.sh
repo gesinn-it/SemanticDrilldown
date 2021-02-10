d5605acf7eee01385b27ac202ba21495fbb330e6
Alexander Gesinn <alexander.gesinn@gesinn.it>
2021-02-10 11:36:15 +0100

    install PHPUnit via Composer

--- build/travis/install-semantic-drilldown.sh
+++ build/travis/install-semantic-drilldown.sh
@@ -6,8 +6,12 @@ MW_INSTALL_PATH=$BASE_PATH/../mw
 
 cd $MW_INSTALL_PATH/extensions
 git clone https://gerrit.wikimedia.org/r/mediawiki/extensions/SemanticDrilldown.git
-ls -la
-cd ..
 
+if [ "$PHPUNIT" != "" ]
+then
+	composer require 'phpunit/phpunit='$PHPUNIT --update-with-dependencies
+fi
+
+cd ..
 echo 'require_once "$IP/extensions/SemanticDrilldown/SemanticDrilldown.php";' >> LocalSettings.php
-cat LocalSettings.php
+php maintenance/update.php --skip-external-dependencies --quick
