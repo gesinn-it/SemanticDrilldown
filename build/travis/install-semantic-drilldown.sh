53f4ec8a4147f72b82419a031ce14c4ee0212940
Alexander Gesinn <alexander.gesinn@gesinn.it>
2021-02-10 11:01:49 +0100

    attempt fix install script, add debug output

--- build/travis/install-semantic-drilldown.sh
+++ build/travis/install-semantic-drilldown.sh
@@ -4,7 +4,10 @@ set -ex
 BASE_PATH=$(pwd)
 MW_INSTALL_PATH=$BASE_PATH/../mw
 
-cd $MW_INSTALL_PATH
-
+cd $MW_INSTALL_PATH/extensions
 git clone https://gerrit.wikimedia.org/r/mediawiki/extensions/SemanticDrilldown.git
+ls -la
+cd ..
+
 echo 'require_once "$IP/extensions/SemanticDrilldown/SemanticDrilldown.php";' >> LocalSettings.php
+cat LocalSettings.php
